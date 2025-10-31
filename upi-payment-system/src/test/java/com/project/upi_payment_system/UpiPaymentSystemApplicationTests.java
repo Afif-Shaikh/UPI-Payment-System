c1985d59e191b238ba9303fccf1c219a240271a7
Afif-Shaikh <shaikhafif5@gmail.com>
2025-10-31 21:16:54 +0530

    Replace old code with new working project

---
+++ upi-payment-system/src/test/java/com/project/upi_payment_system/UpiPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.upi_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
